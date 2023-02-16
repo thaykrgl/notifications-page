ea5ce1354bf088d218f195085b25f1acce7f8f8f
Taha AYKIROĞLU <taha.aykiroglu@karaca.com>
2023-02-16 21:07:14 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ markAll.addEventListener("click", function () {
     let notifications = document.getElementsByClassName("notification");
     let notificationNumber = document.getElementById("notification-number").innerHTML;
 
-    console.log(notificationNumber);
-
     for (let i = 0; i < notifications.length; i++) {
         if (notifications[i].classList.contains("unread")) {
             notifications[i].classList.remove("unread");
