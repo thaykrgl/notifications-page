43cb6836cd0b5c230d01b5491f1a55289fedd7c2
Taha <tahaaykiroglu99@gmail.com>
2024-09-10 09:44:37 +0300

    refactor: prettier & scroll fix

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let markAll = document.getElementById("mark-all");
 
 markAll.addEventListener("click", function () {
-    let notifications = document.getElementsByClassName("notification");
-    let notificationNumber = document.getElementById("notification-number").innerHTML;
+  let notifications = document.getElementsByClassName("notification");
+  let notificationNumber = document.getElementById(
+    "notification-number"
+  ).innerHTML;
 
-    for (let i = 0; i < notifications.length; i++) {
-        if (notifications[i].classList.contains("unread")) {
-            notifications[i].classList.remove("unread");
-            notifications[i].querySelector(".info-container .detail-info .unread-icon").classList.remove("unread-icon");
-            notificationNumber = notificationNumber - 1;
-        }
+  for (let i = 0; i < notifications.length; i++) {
+    if (notifications[i].classList.contains("unread")) {
+      notifications[i].classList.remove("unread");
+      notifications[i]
+        .querySelector(".info-container .detail-info .unread-icon")
+        .classList.remove("unread-icon");
+      notificationNumber = notificationNumber - 1;
     }
+  }
 
-    document.getElementById("notification-number").innerHTML = notificationNumber;
-})
+  document.getElementById("notification-number").innerHTML = notificationNumber;
+});
